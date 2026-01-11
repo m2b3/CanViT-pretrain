41a377c9cdb71ada7b6c8ca27e2dfa13c70811c6
Yohaï-Eliel Berreby <me@yberreby.com>
2026-01-11 18:40:54 -0500

    lower peak LR to 4e-4, empirically good

--- avp_vit/train/config.py
+++ avp_vit/train/config.py
@@ -30,7 +30,7 @@ class Config:
     batch_size: int = 64
     warmup_steps: int = 100_000
     start_lr: float | None = 1e-7  # None = peak_lr / warmup_steps (old behavior)
-    peak_lr: float = 5e-4
+    peak_lr: float = 4e-4
     end_lr: float | None = 1e-6  # None = 0 (old behavior)
     # weight_decay: float = 0.05  # standard in ViTs
     # we can use a much lower weight decay due to the richness of our training signal
